64337080424049c280a505b28c18ded7f0257d41
Vitor Saturi <vitorsaturi21@gmail.com>
2026-03-29 23:58:34 -0300

    Update TestaVetorPolimorfico.java

--- AtividadesJava/src/Heranca/TestaVetorPolimorfico.java
+++ AtividadesJava/src/Heranca/TestaVetorPolimorfico.java
@@ -6,8 +6,8 @@ public class TestaVetorPolimorfico {
         Desenvolvedor[] equipe = new Desenvolvedor[4];
 
         equipe[0] = new Junior("Carlos Silva", "Alice", "Java", 3500.0f);
-        equipe[1] = new Pleno("Bob", "Python", 6500.0f, 12); // 12 projetos entregues
-        equipe[2] = new Senior("Carol", "C#", 12000.0f, 2500.0f); // 2500 de verba
+        equipe[1] = new Pleno("Bob", "Python", 6500.0f, 12);
+        equipe[2] = new Senior("Carol", "C#", 12000.0f, 2500.0f); 
         equipe[3] = new Desenvolvedor("Dave", "PHP", 3000.0f);
 
         System.out.println("=== RELATÓRIO DE DESENVOLVEDORES ===\n");
